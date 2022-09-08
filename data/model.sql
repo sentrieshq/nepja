3670d10e4a9c1acced1aa3ca798f8ea5676058f9
Kollan House <kollanh@gmail.com>
2022-09-07 17:06:31 -0700

    feat: adjusted for more generalized use case

--- data/model.sql
+++ data/model.sql
@@ -1,21 +1,21 @@
 -- Table Definition ----------------------------------------------
 
-CREATE TABLE sentries (
-    sentry_id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+CREATE TABLE nfts (
+    nft_id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     stake_account_id bigint REFERENCES stake_accounts(stake_account_id),
-    sentry_address text NOT NULL UNIQUE,
-    sentry_owner_address text NOT NULL,
-    sentry_create_time timestamp with time zone NOT NULL DEFAULT now(),
-    sentry_update_time timestamp with time zone NOT NULL,
-    sentry_details jsonb,
-    sentry_name text
+    nft_address text NOT NULL UNIQUE,
+    nft_owner_address text NOT NULL,
+    nft_create_time timestamp with time zone NOT NULL DEFAULT now(),
+    nft_update_time timestamp with time zone NOT NULL,
+    nft_details jsonb,
+    nft_name text
 );
 
 -- Indices -------------------------------------------------------
 
-CREATE UNIQUE INDEX sentries_pkey ON sentries(sentry_id int8_ops);
-CREATE UNIQUE INDEX sentries_sentry_address_key ON sentries(sentry_address text_ops);
-CREATE INDEX sentries_sentry_owner_address_idx ON sentries(sentry_owner_address text_ops);
+CREATE UNIQUE INDEX nfts_pkey ON nfts(nft_id int8_ops);
+CREATE UNIQUE INDEX nfts_nft_address_key ON nfts(nft_address text_ops);
+CREATE INDEX nfts_nft_owner_address_idx ON nfts(nfts_owner_address text_ops);
 
 -- Table Definition ----------------------------------------------
 
@@ -24,6 +24,7 @@ CREATE TABLE stake_accounts (
     stake_account_address text NOT NULL UNIQUE,
     stake_account_stake_authority text NOT NULL,
     stake_account_withdraw_authority text NOT NULL,
+    stake_account_voter_authority text NOT NULL,
     stake_account_stake numeric,
     stake_account_active boolean DEFAULT false,
     stake_account_create_time timestamp with time zone NOT NULL DEFAULT now(),
@@ -40,18 +41,18 @@ CREATE INDEX stake_accounts_stake_account_withdraw_authority_idx ON stake_accoun
 
 -- Table Definition ----------------------------------------------
 
-CREATE TABLE sentries_vest (
-    sentry_vest_id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    sentry_address text NOT NULL UNIQUE,
-    sentry_owner_address text NOT NULL,
-    sentry_vest_thawed boolean NOT NULL DEFAULT false,
-    sentry_vest_thawed_time timestamp with time zone,
-    sentry_vest_details jsonb,
-    sentry_vest_message_sent boolean DEFAULT false
+CREATE TABLE nfts_vest (
+    nft_vest_id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    nft_address text NOT NULL UNIQUE,
+    nft_owner_address text NOT NULL,
+    nft_vest_thawed boolean NOT NULL DEFAULT false,
+    nft_vest_thawed_time timestamp with time zone,
+    nft_vest_details jsonb,
+    nft_vest_message_sent boolean DEFAULT false
 );
 
 -- Indices -------------------------------------------------------
 
-CREATE UNIQUE INDEX sentries_vest_pkey ON sentries_vest(sentry_vest_id int8_ops);
-CREATE UNIQUE INDEX sentries_vest_sentry_address_key ON sentries_vest(sentry_address text_ops);
-CREATE INDEX sentries_vest_sentry_owner_address_idx ON sentries_vest(sentry_owner_address text_ops);
+CREATE UNIQUE INDEX nfts_vest_pkey ON nfts_vest(nft_vest_id int8_ops);
+CREATE UNIQUE INDEX nfts_vest_nft_address_key ON nfts_vest(nft_address text_ops);
+CREATE INDEX nfts_vest_nft_owner_address_idx ON nfts_vest(nft_owner_address text_ops);
